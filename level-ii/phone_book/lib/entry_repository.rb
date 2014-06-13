930bf93a483bd0f425ca12b37e0ef3444903a8fb
timproctor <timandcamiproctor@gmail.com>
2014-06-13 12:29:39 -0600

    saving current

--- level-ii/phone_book/lib/entry_repository.rb
+++ level-ii/phone_book/lib/entry_repository.rb
@@ -3,9 +3,11 @@ class EntryRepository
     new
   end
 
+  attr_reader :people, :phone_numbers
   def initialize(people, phone_numbers)
+    @people = people
+    @phone_numbers = phone_numbers
   end
-
   def find_by_last_name(name)
     []
   end
